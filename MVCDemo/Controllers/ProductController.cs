a1eb25b71cd219f3078ac6e504a6fb43812b9a42
Victor Salinas <victorsalinas6@gmail.com>
2024-01-19 14:29:34 -0600

    Finished the View, Update, Create, Delete product

---
+++ MVCDemo/Controllers/ProductController.cs
@@ -0,0 +1,63 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MVCDemo.Models;
+
+namespace MVCDemo.Controllers
+{
+    public class ProductController : Controller
+    {
+
+        private readonly IProductRepo _repo;
+
+        public ProductController(IProductRepo repo)
+        {
+            _repo = repo;
+        }
+
+        public IActionResult Index()
+        {
+            var products = _repo.GetAllProducts();
+            return View(products);
+        }
+
+        public IActionResult ViewProduct(int id)
+        {
+            var product = _repo.GetProduct(id);
+            return View(product);
+        }
+
+        public IActionResult UpdateProduct(int id) 
+        {
+            Product prod = _repo.GetProduct(id);
+            if (prod == null)
+            {
+                return View("ProductNotFound");
+            }
+            return View(prod);
+        }
+
+        public IActionResult UpdateProductToDataBase(Product product)
+        {
+            _repo.UpdateProduct(product);
+
+            return RedirectToAction("ViewProduct",new { id = product.ProductID });
+        }
+
+        public IActionResult InsertProduct()
+        {
+            var prod = _repo.AssignCategory();
+            return View(prod);
+        }
+
+        public IActionResult InsertProductToDatabase(Product productToInsert)
+        {
+            _repo.InsertProduct(productToInsert);
+            return RedirectToAction("Index");
+        }
+
+        public IActionResult DeleteProduct(Product productToDelete) 
+        { 
+            _repo.DeleteProduct(productToDelete);
+            return RedirectToAction("Index");
+        }
+    }
+}
